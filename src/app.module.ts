92cfb8d0d2cd25149eefa0a2ef85d38eb7cbcd7f
Park-Jeong-Ki <jgpark1998@gmail.com>
2023-04-09 02:46:01 +0900

    2.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { RestuarantModule } from './restuarant/restuarant.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    RestuarantModule,
+  ],
   controllers: [],
   providers: [],
 })
