d9cc16e5c9263db16b834d0f425472e743cc8d21
Park-Jeong-Ki <jgpark1998@gmail.com>
2023-07-21 16:22:11 +0900

    NODE_ENV 까지 구축 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,15 @@ import { join } from 'path';
 import { RestuarantModule } from './restuarant/restaurant.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+    }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       autoSchemaFile: true,
       driver: ApolloDriver,
@@ -14,11 +20,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     RestuarantModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5433,
-      username: 'jeongpa',
-      password: '190124',
-      database: 'nuber-eats',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       logging: true,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
