e12a2e4a72326ef83f17406eb1b0f503c60e6782
Park-Jeong-Ki <jgpark1998@gmail.com>
2023-07-21 16:01:46 +0900

    TypeORMMODULE 연결 성공

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { RestuarantModule } from './restuarant/restaurant.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -11,6 +12,17 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
       driver: ApolloDriver,
     }),
     RestuarantModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'jeongpa',
+      password: '190124',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    }),
   ],
   controllers: [],
   providers: [],
